bf41c6845ab94f81fb20905973b0bce360762bd0
Marysia Holenko <maria.holenko@gmail.com>
2015-07-10 14:04:00 +0200

    Getting rid of repetitons in static_pages_controller_test.rb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,28 +7,32 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Home | Agroturystyka Radolinek 39"
   end
 
+    def setup
+        @base_title = "Agroturystyka Radolinek 39"
+    end
+
   test "should get atrakcje" do
     get :atrakcje
     assert_response :success
-    assert_select "title", "Atrakcje | Agroturystyka Radolinek 39"
+    assert_select "title", "Atrakcje | #{@base_title}"
   end
 
   test "should get cennik" do
     get :cennik
     assert_response :success
-    assert_select "title", "Cennik | Agroturystyka Radolinek 39"
+    assert_select "title", "Cennik | #{@base_title}"
   end
 
   test "should get kontakt" do
     get :kontakt
     assert_response :success
-    assert_select "title", "Kontakt | Agroturystyka Radolinek 39"
+    assert_select "title", "Kontakt | #{@base_title}"
   end
 
   test "should get galeria" do
     get :galeria
     assert_response :success
-    assert_select "title", "Galeria | Agroturystyka Radolinek 39"
+    assert_select "title", "Galeria | #{@base_title}"
   end
 
 end
